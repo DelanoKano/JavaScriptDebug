9a5d13dd9822b8028bf825765307302430a36d8b
DelanoKano <144001567+DelanoKano@users.noreply.github.com>
2024-03-22 17:28:58 +0100

    ToDo App in make

---
+++ ToDo-APP/script.js
@@ -0,0 +1,11 @@
+const textbox = document.getElementById("main-textbox");
+const button = document.getElementById("main-button");
+const list = document.querySelectorAll(".todo-list, li");
+
+console.log(list);
+
+button.addEventListener("click", function() {
+    list.forEach(function, elem) => {
+        
+    });
+})
